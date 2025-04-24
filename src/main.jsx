05ade752d8aa5ad625c4c024e07a2099a92d497d
Jake Peters <pa.jacob.peters@gmail.com>
2025-04-24 10:12:09 -0700

    Vercel Analytics Addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   </BrowserRouter>
 );
